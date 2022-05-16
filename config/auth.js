7b40a45329a4f08e02594a8040605ccbb7be9346
Godwin Idemudia <godwinide@gmail.com>
2022-05-16 23:51:30 +0100

    forst commit

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash('error_msg', 'Login required');
+      res.redirect('/signin');
+    },
+    ensureAdmin: function(req, res, next) {
+      if (req.user?.isAdmin) {
+        return next();
+      }
+      req.flash('error_msg', 'Login required');
+      res.redirect('/admin/signin');
+    },
+    forwardAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/');      
+    }
+  };
